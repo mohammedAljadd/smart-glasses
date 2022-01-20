5e18bdb3afbbff2def2e789687c3cbc55ad2ee29
mohammedAljadd <aljadd3@gmail.com>
2022-01-20 13:17:33 +0100

    facial recognition

--- app/resources.py
+++ app/resources.py
@@ -10,7 +10,43 @@ import pathlib
 
 class Facial_Recognition(Resource):
     def post(self):
-        return {"msg": "Facial_Recognition"}
+        if request.files:
+            
+            from app import app
+            imgs_path = app.config['IMAGE_FOLDER']
+
+            # Face cascade xml file
+            cascades = app.config['CASCADE_FOLDER']
+            face_cascade = cv2.CascadeClassifier(os.path.join(cascades, 'haarcascade_frontalface_default.xml'))
+            
+            # Make upload folder empty first
+            empty_folder(imgs_path)
+
+            # Save the image
+            image = request.files["image"]
+            image_name = image.filename
+            image_path = imgs_path+image_name
+            image.save(image_path)
+            
+            # Grayscale
+            gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+            
+            # Detect the face with face_cascade
+            faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=5)
+            for (x, y, w, h) in faces:
+                face = gray_image[y:y+h, x:x+w]
+                IMG_SIZE = 128 
+                face_resized = cv2.resize(face, (IMG_SIZE, IMG_SIZE)) 
+                face_expanded = np.expand_dims(face_resized, axis=0)
+
+                # Rescale the image, the model was trained on scaled images
+                face_input = face_expanded/255.0
+
+                # Make prediction
+                prediction = predict(face_input, threshold=0.6)
+            
+            return prediction
 
 class Object_Detection(Resource):
     def post(self):
