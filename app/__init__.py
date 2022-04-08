5ad8aa833ae867ec8d098c6a5e27ad5fc8a48463
mohammedAljadd <aljadd3@gmail.com>
2022-04-08 09:01:37 +0200

    test class removed

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ api = Api(app)
 
 
 # Adding resources to RestAPI
-api.add_resource(Facial_Recognition, "/facialrecogntion")
+api.add_resource(Facial_Recognition, "/facialrecognition")
 api.add_resource(Object_Detection, "/objectdetection")
 api.add_resource(Text_recognition, "/textrecognition")
 api.add_resource(Text_recognition_HTR, "/textrecognitionHTR")
