90517f65d3af3d4cd8011bb416095de27d538ec8
mohammedAljadd <aljadd3@gmail.com>
2022-04-25 09:44:05 +0200

    server sock

--- 2nd version/server.py
+++ 2nd version/server.py
@@ -12,7 +12,7 @@ server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 host_name  = socket.gethostname()
 host_ip = socket.gethostbyname(host_name)
-port = 9999
+port = 1439
 
 socket_address = (host_ip,port)
 
